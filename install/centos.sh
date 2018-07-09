ed27e7e5e35d656b8640a5b9189fc1482da1f16c
Felix Liberio <iknaxio@users.noreply.github.com>
2018-07-09 18:49:38 -0500

    CentOS install

---
+++ install/centos.sh
@@ -0,0 +1,6 @@
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $(whoami)
