d05872d1d99cd864d0d6fabc56a6d63653f80b8e
Felix Liberio <iknaxio@users.noreply.github.com>
2018-08-18 13:30:33 -0500

    Update ubuntu.sh

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -1,8 +1,8 @@
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/source.list.d/docker.list
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce
 sudo systemctl enable docker
 sudo usermod -aG docker $(whoami)
